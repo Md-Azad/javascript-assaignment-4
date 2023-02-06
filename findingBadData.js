664a1e8f1165ec96b98a1f9201c8b62d87a072ec
Md-Azad <sokalazad@gmail.com>
2023-02-06 19:20:40 +0100

    solved bad data problem

---
+++ findingBadData.js
@@ -0,0 +1,25 @@
+
+
+const data = [-4,-9,-5,-33,-55];
+
+function findingBadData(dataArray){
+
+    let badData = [];
+
+
+    if(Array.isArray(dataArray)  !== true){
+        return "Please insert an array.";
+    }
+    else{
+        for(let i =0; i<dataArray.length; i++){
+            if(dataArray[i]<0){
+                badData.push(data[i]);
+            }
+
+        }
+        return badData.length;
+    }
+
+}
+
+console.log(findingBadData(data));
